cf5c0887fddac03c56011947ef0ff1295d5c7951
Sumit Kalra <skalra896@gmail.com>
2022-12-09 02:52:26 -0500

    working write

---
+++ client_serv_handler/Handler.thrift
@@ -0,0 +1,12 @@
+namespace py Handler
+
+typedef i32 int 
+service Handler{
+    
+    void write(1:int n1, 2:int n2)
+    void ack(1:int n1)
+    void writeSuccessor(1:int n1, 2:int n2)
+    void set_node_connections(1:int n1)
+     
+    
+}
--- client_serv_handler/server.py
+++ client_serv_handler/server.py
@@ -1,94 +1,117 @@
-class ServerHandler:
-    port = 9090
-    server_ips = ["10.10.1.1", "10.10.1.2", "10.10.1.3"]
-    index = 0
+
+port = 9090
+
+from operator import index
+import sys
+# your gen-py dir
+sys.path.append('gen-py')
+
+import time
+
+from Handler import *
+from Handler.ttypes import *
+
+# Thrift files
+from thrift.transport import TSocket
+from thrift.transport import TTransport
+from thrift.protocol import TBinaryProtocol
+from thrift.server import TServer
+
+class ServiceHandler:
     
+    server_ips = ["155.98.39.2", "155.98.39.3", "155.98.39.4"]# node 0,1,2
+
     def __init__(self): 
         self.length = len(self.server_ips)
         self.map = {} 
         self.next = None
         self.prev = None
         self.tail = None
-        if self.index != self.length - 1: #not tail
+
+    
+    def set_node_connections(self, index):
+        self.index = index
+        
+        print('inside set node connections method ')
+
+        if self.index != self.length - 1 : #not tail
+            print('making next connection set node connections, index: %s '% (self.index))
             self.next = self.makeConnection(self.server_ips[self.index + 1]) 
-        if self.index != 0:       #not head
+            print('connected %s to %s as next node' %(self.server_ips[self.index], self.server_ips[self.index+1]))
+    
+        
+        if self.index != 0 :       #not head
+            print('making prev connection set node connections, index: %s '% (self.index))
             self.prev = self.makeConnection(self.server_ips[self.index - 1])
+            print('connected %s to %s as prev node' %(self.server_ips[self.index], self.server_ips[self.index-1]))
+
+        if self.index != self.length - 1: 
+            print('making tail connection set node connections, index: %s '% (self.index))
+            self.tail = self.makeConnection(self.server_ips[self.length - 1]) 
+            print('connected %s to %s as tail node' %(self.server_ips[self.index], self.server_ips[self.length-1]))
+
+
+    def makeConnection(self, host): 
+        try: 
+            # Init thrift connection and protocol handlers
             
-        self.tail = self.makeConnection(self.server_ips[self.self.length - 1]) 
+            transport = TSocket.TSocket(host , port)
+            transport = TTransport.TBufferedTransport(transport)
+            protocol = TBinaryProtocol.TBinaryProtocol(transport)
             
+            client = Handler.Client(protocol)
+            
+            # Connect to server
+            transport.open()
+            print('connected to host : %s' %(host))
+            
+        except Thrift.TException as tx:
+                print('openSocket error : %s, host: %s, port: %s' 
+                        % (tx.message, host, port)) # TODO add host and port 
+  
+        return client
 
     def write(self, key, val):
-
-        self.node.map[key] = {"msg" : val, "dirtybit" : 1} #data is dirty
+        print('making next connection set node connections, index: %s '% (self.index))
+        
+        self.map[key] = {"msg" : val, "dirtybit" : 1} #data is dirty
 
         if self.next != None:                             # have next node
             self.writeSuccessor(key, val)
 
         else:                                              # tail node
-            self.ack(self, key)                             # commit + ack back                            
-            
-        
-    def read(self, key):
-        if self.node.map.get(key) == None: 
-            return None                       #key is not present
-    
-        if(self.node.map[key]["dirtybit"] == 0):   #data is commited at current node
-            return self.node.map[key]["msg"]
-        
-        bitAtTail = self.readTail(key)       #check dirty bita at tail
-        
-        if bitAtTail == 0:                      # data is commited at tail
-            return self.node.map[key]["msg"]        # return data
-        
-        return None                           #tail does not have data
-    
+            self.ack(key)                             # commit + ack back                            
+     
     def ack(self, key):
-        self.node.map[key]["dirtybit"] = 0
-        try:
-            self.node.prev.ack(key)
-        except Thrift.TException as tx:
-            print('writeSuccessor couldnt pass message: %s' % (tx.message))
+        if self.index != 0:
+            print('inside ack method sending from %s to %s ' % (self.server_ips[self.index], self.server_ips[self.index - 1]))
+            self.map[key]["dirtybit"] = 0
+            try:
+                self.prev.ack(key)
+                print('sent ack from %s to %s ' % (self.server_ips[self.index], self.server_ips[self.index - 1]))
+            except Thrift.TException as tx:
+                print('writeSuccessor couldnt pass message: %s' % (tx.message))
 
-  
-    def readTail(self, key): 
-        bit = self.tail.checkDirtybit(key)
-        if bit == 1: 
-            return 1  #uncommited data
-        return 0      #commited data
-                      
-    def checkDirtybit():
-        if self.map.get(key) == None: 
-            return 1                      #data is not present
-        return self.map[key]["dirtybit"]       
-        
+
     def writeSuccessor(self, key, value):
         try:
-            val = self.node.next.write(key, value)
-            return val
+            val = self.next.write(key, value)
 
         except Thrift.TException as tx:
             print('writeSuccessor couldnt pass message: %s' % (tx.message))
-        
-    def makeConnection(self, host): 
-        try: 
-            # Init thrift connection and protocol handlers
-            transport = TSocket.TSocket(host , self.port)
-            transport = TTransport.TBufferedTransport(transport)
-            protocol = TBinaryProtocol.TBinaryProtocol(transport)
-            # Set client to our Example
-            client = Example.Client(protocol)
-            # Connect to server
-            transport.open()
-        except Thrift.TException as tx:
-                print('openSocket error : %s, host: %s, port: %s' 
-                        % (tx.message, host, self.port)) # TODO add host and port 
-        return client
     
-"""
-    def closeSocket(self, tranport):
-        # Close connection
-        try:
-            transport.close()
-        except Thrift.TException as tx:
-                print('closeSocket : %s' % (tx.message)) # TODO add host and port 
-"""
+        
+# set handler to our implementation
+handle = ServiceHandler()
+
+processor = Handler.Processor(handle)
+transport = TSocket.TServerSocket("0.0.0.0", port)
+tfactory = TTransport.TBufferedTransportFactory()
+pfactory = TBinaryProtocol.TBinaryProtocolFactory()
+
+# set server
+server = TServer.TThreadedServer(processor, transport, tfactory, pfactory)
+
+print('Starting server')
+server.serve()
+handle.createChain()
