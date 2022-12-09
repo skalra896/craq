8341887bcc897761e391b96ea0f7eebb25f16ad1
tanu02 <tanubatra02@gmail.com>
2022-12-08 21:36:09 -0500

    update index

--- handler/Handler.thrift
+++ handler/Handler.thrift
@@ -4,9 +4,9 @@ typedef i32 int
 service Handler{
     
     int write(1:int n1, 2:int n2)
-    int ack(1:int n1)
+    void ack(1:int n1)
     int writeSuccessor(1:int n1, 2:int n2)
-    void set_node_connections()
+    void set_node_connections(1:int n1)
      
     
 }
