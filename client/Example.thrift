b44e42d57dbb1f113cc5ac055539ed91b96734a5
Sumit Kalra <skalra896@gmail.com>
2022-12-08 20:44:52 -0500

    updating locations

--- client/Example.thrift
+++
@@ -1,8 +0,0 @@
-namespace py Example
-service Example{
-    // return current time stamp
-    string showCurrentTimestamp()
-     
-    // wait for 10 seconds, but work asynchronously
-    oneway void asynchronousJob()
-}
